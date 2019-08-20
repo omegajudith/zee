e2fe47ba00feff5a9f9781fa2a58fb19e887f436
omegajudith <omegajudith@gmail.com>
2019-08-20 16:04:05 +0300

    evens

--- sign/jdi.py
+++ sign/jdi.py
@@ -1,7 +1,10 @@
-def test(l):
-    x = []
-    for a in l:
-        if a not in x:
-            x.append(a)
-            return x
-            print(test([1,2,3,3,3,3,4,5])
+
+
+def uni(nums):
+  zip = []
+  for item in nums:
+    if item not in zip:
+      zip.append(item)
+  return zip
+
+print(uni([1,2,3,3,3,3,4,5])) 
--- sign/py2.py
+++
@@ -1,3 +0,0 @@
-#create a package and import
-#write a py prog that returns a list and a new list with unique elements of the first list no methods
-#py prog that prints  even nums froma given list
